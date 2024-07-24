0f4db444a94ce28bc942f0e44bffad09f46cc1ea
Ritwik Saha <ritwik@cloudera.com>
2024-07-24 12:35:48 -0400

    fix graph visualization

--- pgs/graph_visualisation.py
+++ pgs/graph_visualisation.py
@@ -20,28 +20,47 @@ def _get_raw_auxillary_context_for_papers(paper_ids: List[str], graphDbInstance:
     }
     CALL {
         WITH p
-        MATCH (p)<-[:CITES]-(cited_by:Paper)<-[:CITES]-(other_paper:Paper)
-        WITH cited_by, COUNT(other_paper) as citation_count
+        MATCH (p)<-[:CITES]-(top_paper:Paper)<-[:CITES]-(other_paper:Paper)
+        WITH top_paper, COUNT(other_paper) as citation_count
         ORDER BY citation_count DESC
         LIMIT 3
-        RETURN cited_by
+        RETURN top_paper
     }
-    RETURN p, cited_by, a
+    RETURN p, top_paper, a
     """.replace("$list", str(paper_ids))
     results = graphDbInstance.query(query)
     return results
 
+def _get_citation_relationships(arxiv_ids: List[str], graphDbInstance: Neo4jGraph):
+    query = r"""
+    MATCH (p:Paper)
+    WHERE p.id in $list
+    WITH COLLECT(ELEMENTID(p)) as paper_ids
+    CALL apoc.algo.cover(paper_ids)
+    YIELD rel
+    WITH COLLECT(rel) AS citations
+    RETURN [r IN citations | [startNode(r).id, endNode(r).id]] AS node_pairs
+    """.replace("$list", str(arxiv_ids))
+    results = graphDbInstance.query(query)
+    return results[0]["node_pairs"]
+
 def _create_networkx_graph(paper_ids: List[str], graphDbInstance: Neo4jGraph):
     data = _get_raw_auxillary_context_for_papers(paper_ids, graphDbInstance)
+    unique_papers = set()
     G = nx.DiGraph()
     for record in data:
-        p, cited_by, author = record['p'], record['cited_by'], record['a']
-        G.add_node(cited_by['id'], label=cited_by['title'], color='salmon')
+        p, top_paper, author = record['p'], record['top_paper'], record['a']
+        unique_papers.update([p['id'], top_paper['id']])
+        G.add_node(top_paper['id'], label=top_paper['title'], color='salmon')
         G.add_node(p['id'], label=p['title'], color='purple')
         G.add_node(author['name'], label=author['name'], color='yellow')
         G.add_edges_from([
-            (cited_by['id'], p['id'], {'label': 'CITES'}),
             (p['id'], author['name'], {'label': 'AUTHORED_BY'}),
+        ])
+    node_pairs = _get_citation_relationships(list(unique_papers), graphDbInstance)
+    for pair in node_pairs:
+        G.add_edges_from([
+            (pair[0], pair[1], {'label': 'CITES'}),
         ])
     return G
 
