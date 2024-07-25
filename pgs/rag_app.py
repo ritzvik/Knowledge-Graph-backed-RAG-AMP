8a5cc89c2c5deb1259f07de820bf90efffc16400
Ritwik Saha <ritwik@cloudera.com>
2024-07-25 18:09:41 -0400

    change formatting

--- pgs/rag_app.py
+++ pgs/rag_app.py
@@ -89,7 +89,7 @@ def generate_responses(input_text):
         answer_followup = h.invoke_followup()
         logging.info("generated follow-up answer")
         hybrid_folllow_up.markdown("### Follow-up details")
-        hybrid_folllow_up.html(f'<span style="font-size:0.8em;">\n{markdown.markdown(linkify_text(answer_followup))}</span>', unsafe_allow_html=True)
+        hybrid_folllow_up.html(f'<span style="font-size:0.8em;">\n{markdown.markdown(linkify_text(answer_followup))}</span>')
 
         status.update(label="Answer Generation Complete", state="complete", expanded=False)
     
